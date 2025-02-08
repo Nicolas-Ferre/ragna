3ed47bfc78b7099fc3aeb4b8e13fc237628d4665
Nicolas Ferré <nicolas.ferre.public@gmail.com>
2025-02-08 12:51:37 +0100

    Increase coverage

--- crates/ragna/tests/integration/types.rs
+++ crates/ragna/tests/integration/types.rs
@@ -1,7 +1,8 @@
 #![allow(clippy::lossy_float_literal)]
 
 use crate::types::gpu::{
-    register, BOOL_FALSE_VALUE, BOOL_TRUE_VALUE, F32_VALUE, I32_VALUE, U32_VALUE,
+    register, BOOL_FALSE_VALUE, BOOL_TRUE_VALUE, F32_FRAC_VALUE, F32_INT_VALUE, I32_VALUE,
+    U32_VALUE,
 };
 use ragna::App;
 
@@ -10,7 +11,8 @@ pub fn assign_values() {
     let app = App::default().with_module(register).run(1);
     assert_eq!(app.read(I32_VALUE), Some(0x7FFF_FFFF));
     assert_eq!(app.read(U32_VALUE), Some(0xFFFF_FFFF));
-    assert_eq!(app.read(F32_VALUE), Some(999_999_999_999_999_999_999.));
+    assert_eq!(app.read(F32_INT_VALUE), Some(999_999_999_999_999_999_999.));
+    assert_eq!(app.read(F32_FRAC_VALUE), Some(123.456));
     assert_eq!(app.read(BOOL_FALSE_VALUE), Some(false));
     assert_eq!(app.read(BOOL_TRUE_VALUE), Some(true));
 }
@@ -19,7 +21,8 @@ pub fn assign_values() {
 mod gpu {
     pub(super) static I32_VALUE: i32 = 0;
     pub(super) static U32_VALUE: u32 = 0;
-    pub(super) static F32_VALUE: f32 = 0.;
+    pub(super) static F32_INT_VALUE: f32 = 0.;
+    pub(super) static F32_FRAC_VALUE: f32 = 0.;
     pub(super) static BOOL_FALSE_VALUE: bool = false;
     pub(super) static BOOL_TRUE_VALUE: bool = false;
 
@@ -27,7 +30,8 @@ mod gpu {
     fn run() {
         I32_VALUE = 0x7FFF_FFFF;
         U32_VALUE = 0xFFFF_FFFF;
-        F32_VALUE = 999_999_999_999_999_999_999.;
+        F32_INT_VALUE = 999_999_999_999_999_999_999.;
+        F32_FRAC_VALUE = 123.456;
         BOOL_FALSE_VALUE = false;
         BOOL_TRUE_VALUE = true;
     }
